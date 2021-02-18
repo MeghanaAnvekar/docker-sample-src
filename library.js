c3bf182d1e8b17705ad4aa6aaa2a4d4cdad5ab27
MeghanaAnvekar <maanvekar@gmail.com>
2021-02-18 17:45:14 +0530

    Create library.js

---
+++ library.js
@@ -0,0 +1,19 @@
+const logger = require('./logger');
+function calculateSquares(data){
+	var nums = data.split("\n");
+	var output = [];
+	var x;
+	var square; 
+
+	for (i = 0; i < nums.length; i++) {
+        x = parseInt(nums[i]);
+        logger.log('Processing ' + x);
+		if (!isNaN(x)){
+			square = x * x;
+			output.push(square);
+		}
+		
+	  }
+    return output;
+}
+module.exports.calculateSquares = calculateSquares;
